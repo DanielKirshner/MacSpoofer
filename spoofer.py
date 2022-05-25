8ccb68531163e205dcfb7c1dfbbbc73f4022e281
DanielKirshner <d.danielkirshner@gmail.com>
2022-05-25 23:05:33 +0300

    Added title for TUI

--- spoofer.py
+++ spoofer.py
@@ -1,7 +1,28 @@
+from rich import print
+import getpass
 
+
+VERSION = "0.0.0"
+
+
+def print_title() -> None:
+    print(
+        "[bold green]"
+        "\t\t\t__\n"
+        " ___ _ __   ___   ___  / _| ___ _ __ \n"
+        "/ __| '_ \ / _ \ / _ \| |_ / _ \ '__|\n"
+        "\__ \ |_) | (_) | (_) |  _|  __/ |\n"
+        "|___/ .__/ \___/ \___/|_|  \___|_|\n"
+        "    |_|\n"
+    )
+
+
+
+def check_for_admin() -> bool:
+    return getpass.getuser() == 'root'
 
 def main() -> None:
-    pass
+    print_title()
 
 if __name__ == "__main__":
     main()
