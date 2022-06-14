caa4acb3daec4a35d59ebd8ee86547ef92c16e3f
DanielKirshner <danielkirshner7878@gmail.com>
2022-06-14 22:24:48 +0300

    Check for administrative privileges

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Check for administrative privileges
+if [[ $EUID -ne 0 ]]; then
+   echo "Needs root." 
+   exit 1
+fi
+
+# Print information
 PROGRAM_VERSION="0.0.2"
 echo "Running setup for spoofer $PROGRAM_VERSION"
