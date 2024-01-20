4a32444d27ca082c61a988a1ca6dce54f9873fc3
DanielKirshner <d.danielkirshner@gmail.com>
2024-01-20 14:04:49 +0200

    Added random mac generate utils

---
+++ src/random_utils.py
@@ -0,0 +1,32 @@
+from enum import Enum
+import random
+
+class HexValuesLength(Enum):
+    MAC_ADDRESS = 12,
+    OUI = 6,
+    NIC = 6
+
+
+def generate_hex_values_delimited_by_dotted(hex_values_to_generate: HexValuesLength) -> str:
+    """
+        Return a random string of hex values delimited by dotted,
+        as the format of a mac address or a part of it.
+
+        Args:
+            hex_values_to_generate (int): Number of values to generate
+
+        Returns:
+            str: Random hex string (for example : "01:bf:e2")
+    """
+    mac = ''
+    
+    for i in range(hex_values_to_generate.value):
+        mac += hex(random.randint(0, 16))[-1].lower()
+    
+    MAC_DELIMITER = ':'
+    mac = MAC_DELIMITER.join([mac[i:i + 2] for i in range(0, len(mac), 2)])
+    
+    return mac
+
+
+print(generate_hex_values_delimited_by_dotted(HexValuesLength.MAC_ADDRESS))
